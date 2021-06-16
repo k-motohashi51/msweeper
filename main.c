04801410b146b071a744b5c2220a9759ed3f55e8
Melony5 <mk41kisa@outlook.jp>
2021-06-16 14:33:59 +0900

    ユーザが地雷の数やボードの大きさを設定できるようにした

--- main.c
+++ main.c
@@ -3,8 +3,6 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define BOARD_SIZE 8
-#define MINE_NUM 10
 
 /* Cell Status */
 #define CLOSED 0  // Cell is still closed
@@ -19,6 +17,21 @@
 #define GAME_OVER 3   // Opend Cell was mine
 #define GAME_CLEAR 4  // All cells was opened
 
+
+/* 設定各種 */
+typedef struct {
+  
+  int   board_size;
+  int   mine_num;
+  int   elapsed_time;
+  int   c_x;        // current_x
+  int   c_y;        // current_y
+  char  c_command;  // current_command
+  int   operate_code;
+
+} settings_t;
+
+/* マスの情報 */
 typedef struct {
 
   int   status;           // Cell status
@@ -27,58 +40,82 @@ typedef struct {
 
 } cell_t;
 
-void initialize(cell_t cell[][BOARD_SIZE]);
+
+cell_t** initialize(settings_t *settings, cell_t **cell);
 void display_navi(void);
-void display_board(cell_t cell[][BOARD_SIZE]);
-void input(int *, int *, char *, cell_t cell[][BOARD_SIZE]);
-bool judge_input(int, int, char, cell_t cell[][BOARD_SIZE]);
-void locate_mine(cell_t cell[][BOARD_SIZE]);
-void count_mine(cell_t cell[][BOARD_SIZE]);
-int  get_mine(int x, int y, cell_t cell[][BOARD_SIZE]);
-int  judge_operation(int, int, char, cell_t cell[][BOARD_SIZE]);
-bool is_clear(cell_t cell[][BOARD_SIZE]);
-void update(int, int, char, cell_t cell[][BOARD_SIZE]);
-void chain(int, int, int, int, cell_t cell[][BOARD_SIZE]);
+void display_board(settings_t settings, cell_t **cell);
+void input(settings_t *settings, cell_t **cell);
+bool judge_input(settings_t settings, cell_t **cell);
+
+void locate_mine(settings_t settings, cell_t **cell);
+void count_mine(settings_t settings, cell_t **cell);
+int  get_mine(int x, int y, settings_t settings, cell_t **cell);
+int  judge_operation(settings_t settings, cell_t **cell);
+bool is_clear(int, int, settings_t settings, cell_t **cell);
+void update(settings_t settings, cell_t **cell);
+void chain(int, int, int, int, int, cell_t **cell);
 void display_result(int operate_code); 
 
 
 int main(void) {
+  
+  settings_t settings;
+  cell_t **cell;
 
-  cell_t cell[BOARD_SIZE][BOARD_SIZE];
-  int   operate_code; 
-  int   c_x, c_y;   // current_x, y
-  char  c_command;  // current_command
+  cell = initialize(&settings, cell);
 
-  initialize(cell);
-
+  printf("init ok\n");
+  
   do {
-    display_board(cell);
+    display_board(settings, cell);
 
-    input(&c_x, &c_y, &c_command, cell);
+    input(&settings, cell);
 
-    operate_code = judge_operation(c_x, c_y, c_command, cell);
+    settings.operate_code = judge_operation(settings, cell);
 
-    update(c_x, c_y, operate_code, cell);
-  } while(operate_code != GAME_OVER && operate_code != GAME_CLEAR);
+    update(settings, cell);
+  } while(settings.operate_code != GAME_OVER && settings.operate_code != GAME_CLEAR);
+
+  for(int i = 0; i < settings.board_size; i++) {
+    free(cell[i]);
+  }
+
+  free(cell);
 
   return 0;
   
 }
 
 /* ゲーム初期化 */
-void initialize(cell_t cell[][BOARD_SIZE]) {
+cell_t** initialize(settings_t *settings, cell_t **cell) {
+  char dummy;
 
-  for(int i = 0; i < BOARD_SIZE; i++) {
-    for(int j = 0; j < BOARD_SIZE; j++) {
+  printf("ボードの大きさ\t>");
+  scanf("%d%c", &(settings->board_size), &dummy);
+
+  printf("地雷の個数\t>");
+  scanf("%d", &(settings->mine_num));
+  printf("OK..\n");
+  
+  // 構造体cellのメモリ確保
+  cell = (cell_t **)malloc(sizeof(cell_t *) * settings->board_size);
+  for(int i = 0; i < settings->board_size; i++) {
+    cell[i] = (cell_t *)malloc(sizeof(cell_t) * settings->board_size);
+  }
+
+  for(int i = 0; i < settings->board_size; i++) {
+    for(int j = 0; j < settings->board_size; j++) {
       cell[i][j].status = CLOSED;
       cell[i][j].is_mine = false;
     }
   }
 
-  locate_mine(cell);
-  count_mine(cell);
+  locate_mine(*settings, cell);
+  count_mine(*settings, cell);
 
   display_navi();
+
+  return cell;
 
 }
 
@@ -95,23 +132,25 @@ void display_navi(void) {
 }
 
 /* 盤面表示 */
-void display_board(cell_t cell[][BOARD_SIZE]) {
+void display_board(settings_t settings, cell_t **cell) {
+  int board_size = settings.board_size;
 
   printf("\n ");
 
-  for(int i = 0; i < BOARD_SIZE; i++) {
+  for(int i = 0; i < board_size; i++) {
     printf(" %d", i); // display x coordinate
   }
 
   printf("\n");
 
-  for(int y = 0; y < BOARD_SIZE; y++) {
+  for(int y = 0; y < board_size; y++) {
     printf("%d", y);  // display y coordinate
-    for(int x = 0; x < BOARD_SIZE; x++) {
+    for(int x = 0; x < board_size; x++) {
       switch(cell[y][x].status) {
         case OPENED:  printf(" %d", cell[y][x].around_mine_num);
                       break;
-        case CLOSED:  printf(" .");
+        case CLOSED:  if(cell[y][x].is_mine == true) printf(" @"); else
+                      printf(" .");
                       break;
         case MARKED:  printf(" M");
                       break;
@@ -127,24 +166,28 @@ void display_board(cell_t cell[][BOARD_SIZE]) {
 }
 
 /* 入力 */
-void input(int *x, int *y, char *command, cell_t cell[][BOARD_SIZE]) {
+void input(settings_t *settings, cell_t **cell) {
 
   char meta;  // dummy input
   bool is_inputtable = false;
 
   while(is_inputtable == false) {
     printf(">");
-    scanf("%d%c%d%c%c%c", x, &meta, y, &meta, command, &meta);
+    scanf("%d%c%d%c%c%c", &(settings->c_x), &meta, &(settings->c_y), &meta, &(settings->c_command), &meta);
 
-    is_inputtable = judge_input(*x, *y, *command, cell);
+    is_inputtable = judge_input(*settings, cell);
   }
 
 }
 
 /* 入力条件を満たしているか判定する */
-bool judge_input(int x, int y, char command, cell_t cell[][BOARD_SIZE]) {
+bool judge_input(settings_t settings, cell_t **cell) {
+  int x = settings.c_x;
+  int y = settings.c_y;
+  int command = settings.c_command;
+  int board_size = settings.board_size;
 
-  if(x < 0 || 7 < x || y < 0 || 7 < y) {
+  if(x < 0 || board_size - 1 < x || y < 0 || board_size - 1 < y) {
     printf("Out of range.\n");
     return false;
   }
@@ -166,19 +209,19 @@ bool judge_input(int x, int y, char command, cell_t cell[][BOARD_SIZE]) {
 
   return true;
 }
-
+/********************************************************************************/
 /* 地雷の場所を決定する */
-void locate_mine(cell_t cell[][BOARD_SIZE]) {
+void locate_mine(settings_t settings, cell_t **cell) {
 
   int r_x, r_y; // random location of mine 
 
   srand((unsigned)time(NULL));
 
-  for(int i = 0; i < MINE_NUM; i++) {
+  for(int i = 0; i < settings.mine_num; i++) {
     // locate mine without duplication
     do {
-      r_x = rand() % 8;
-      r_y = rand() % 8;
+      r_x = rand() % settings.mine_num;
+      r_y = rand() % settings.mine_num;
     } while(cell[r_y][r_x].is_mine == true);
 
     cell[r_y][r_x].is_mine = true;
@@ -187,27 +230,28 @@ void locate_mine(cell_t cell[][BOARD_SIZE]) {
 }
 
 /* 盤面各々のマスの周囲の地雷数を数える */
-void count_mine(cell_t cell[][BOARD_SIZE]) {
+void count_mine(settings_t settings, cell_t **cell) {
 
-  for(int y = 0; y < BOARD_SIZE; y++) {
-    for(int x = 0; x < BOARD_SIZE; x++) {
-      cell[y][x].around_mine_num = get_mine(x, y, cell);
+  for(int y = 0; y < settings.board_size; y++) {
+    for(int x = 0; x < settings.board_size; x++) {
+      cell[y][x].around_mine_num = get_mine(x, y, settings, cell);
     }
   }
 
 }
 
 /* 隣接しているマスの地雷数を数える */
-int get_mine(int x, int y, cell_t cell[][BOARD_SIZE]) {
+int get_mine(int x, int y, settings_t settings, cell_t **cell) {
   int around_mine_num = 0;
+  int board_size = settings.board_size;
 
   for(int i = -1; i < 2; i++) {
-    if(y + i < 0 || 7 < y + i) {
+    if(y + i < 0 || board_size - 1 < y + i) {
       continue;
     }
 
     for(int j = -1; j < 2; j++) {
-      if(x + j < 0 || 7 < x + j) {
+      if(x + j < 0 || board_size - 1 < x + j) {
         continue;
       }
 
@@ -224,7 +268,10 @@ int get_mine(int x, int y, cell_t cell[][BOARD_SIZE]) {
 }
 
 /* 入力された操作の種類の判断 */
-int judge_operation(int x, int y, char command, cell_t cell[][BOARD_SIZE]) {
+int judge_operation(settings_t settings, cell_t **cell) {
+  int x = settings.c_x;
+  int y = settings.c_y;
+  char command = settings.c_command;
 
   if(command == 'm') {
     return MARK;
@@ -233,8 +280,8 @@ int judge_operation(int x, int y, char command, cell_t cell[][BOARD_SIZE]) {
   if(cell[y][x].is_mine == true) {
     return GAME_OVER;
   }
-
-  if(is_clear(cell) == true) {
+  
+  if(is_clear(x, y, settings, cell) == true) {
     return GAME_CLEAR;
   }
 
@@ -246,14 +293,24 @@ int judge_operation(int x, int y, char command, cell_t cell[][BOARD_SIZE]) {
     return OPEN;
   }
 
+  exit(1);
+
 }
 
 /* ゲームクリア条件を満たしているか判定する */
-bool is_clear(cell_t cell[][BOARD_SIZE]) {
+bool is_clear(int x, int y, settings_t settings, cell_t **cell) {
 
-  for(int y = 0; y < BOARD_SIZE; y++) {
-    for(int x = 0; x < BOARD_SIZE; x++) {
-      if(cell[y][x].status == CLOSED) {
+  for(int i = 0; i < settings.board_size; i++) {
+    for(int j = 0; j < settings.board_size; j++) {
+      if(i == y && j == x) {
+        continue;
+      }
+
+      if(cell[i][j].is_mine == true) {
+        continue;
+      }
+
+      if(cell[i][j].status == CLOSED) {
         return false;
       }
     }
@@ -264,11 +321,14 @@ bool is_clear(cell_t cell[][BOARD_SIZE]) {
 }
 
 /* 情報を更新する */
-void update(int x, int y, char operate_code, cell_t cell[][BOARD_SIZE])  {
+void update(settings_t settings, cell_t **cell)  {
+  int x = settings.c_x;
+  int y = settings.c_y;
+  int board_size = settings.board_size;
   
-  switch(operate_code) {
+  switch(settings.operate_code) {
     case OPEN:        cell[y][x].status = OPENED; break;
-    case CHAIN:       chain(x, y, x, y, cell);    break;
+    case CHAIN:       chain(board_size, x, y, x, y, cell);    break;
     case MARK:        cell[y][x].status = MARKED; break;
     case GAME_OVER:   display_result(GAME_OVER);  break;
     case GAME_CLEAR:  display_result(GAME_CLEAR); break;
@@ -278,7 +338,7 @@ void update(int x, int y, char operate_code, cell_t cell[][BOARD_SIZE])  {
 }
 
 /* 入力マスの周囲に地雷がない時のチェイン */
-void chain(int x, int y, int p_x, int p_y, cell_t cell[][BOARD_SIZE]) {
+void chain(int board_size, int x, int y, int p_x, int p_y, cell_t **cell) {
   int f_x, f_y; // forward_x, y
 
   cell[y][x].status = SPACED;
@@ -286,19 +346,19 @@ void chain(int x, int y, int p_x, int p_y, cell_t cell[][BOARD_SIZE]) {
   for(int i = -1; i < 2; i++) {
     f_y = y + i;
 
-    if(f_y < 0 || 7 < f_y)  continue;
+    if(f_y < 0 || board_size - 1 < f_y)  continue;
 
     for(int j = -1; j < 2; j++) {
       f_x = x + j;
 
-      if(f_x < 0 || 7 < f_x)  continue;
+      if(f_x < 0 || board_size - 1 < f_x)  continue;
       if(i == 0 && j == 0)    continue;
       if(p_x == f_x && p_y == f_y)        continue;
       if(cell[f_y][f_x].status == SPACED) continue;
       if(cell[f_y][f_x].status == OPENED) continue;
 
       if(cell[f_y][f_x].around_mine_num == 0) {
-        chain(f_x, f_y, x, y, cell);
+        chain(board_size, f_x, f_y, x, y, cell);
       } else {
         cell[f_y][f_x].status = OPENED;
       }
